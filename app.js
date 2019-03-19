b4a26798a2a41d388c26ac7b45e9c7fef1c65b61
Kunal Puri <kpuri@vt.edu>
2019-03-18 21:05:14 -0400

    Attempt at integrated Mongoose to save data

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+var mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/cs2304project");
+
+var dataSchema = new mongoose.Schema({
+    id: String,
+    posttime: Date.now(),
+    author: { email: String, name: String },
+    message: String
+   });
+
+var User = mongoose.model("User", dataSchema)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
